fecfd22a03e39dba9eadfb6b577fb00a6220ef8d
luishporras <luishercep@gmail.com>
2022-04-27 02:28:37 +0200

    Remove unused styles

--- src/pages/Report.js
+++ src/pages/Report.js
@@ -1,24 +1,20 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import { Container, Typography, Grid } from '@material-ui/core'
+// import { makeStyles } from '@material-ui/core/styles'
+import { Container, Grid } from '@material-ui/core'
 import Page from '../components/Page'
 import ReportCaseForm from '../components/ReportCaseForm'
 // import MetamaskButton from '../../components/MetamaskButton'
 
-const useStyles = makeStyles(theme => ({
-    appBarSpacer: theme.mixins.toolbar,
-    flexItemEnd: {
-        display: 'flex',
-        justifyContent: 'flex-end',
-        alignItems: 'flex-start',
-    },
+// const useStyles = makeStyles(theme => ({
+//     flexItemEnd: {
+//         display: 'flex',
+//         justifyContent: 'flex-end',
+//         alignItems: 'flex-start',
+//     }
+// }))
 
-  
-}))
-
-export default function IssueBadge() {
-    const classes = useStyles()
-
+export default function Report() {
+    // const classes = useStyles()
 
     return (
         <Page container={false}>
@@ -29,8 +25,8 @@ export default function IssueBadge() {
             </Container>
         </Page>
 
-        //                 {/* <Grid item xs={3} className={classes.flexItemEnd}>
-        //                     <MetamaskButton />
-        //                 </Grid> */}
+        // {<Grid item xs={3} className={classes.flexItemEnd}>
+        //     <MetamaskButton />
+        // </Grid> }
     )
 }
